c975c20a5bd0dbea3ab5fb88dd73fc638c26b57f
Akash Sachan <akashsachan@Akashs-MacBook-Air.local>
2022-05-31 23:19:16 +0530

    Final

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -10,18 +10,21 @@ import {
   HomeIcon,
 } from '@heroicons/react/outline'
 import SidebarRow from './SidebarRow'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 function Sidebar() {
+  const {data: session} = useSession()
+
   return (
-    <div>
-        <img className='h-10 w-10' src="https://links.papareact.com/drg" alt="" />
+    <div className='col-span-2 flex flex-col items-center px-4 md:items-start m-3'>
+        {/* <img className='h-10 w-10' src="https://links.papareact.com/drg" alt="" /> */}
         <SidebarRow Icon={HomeIcon} title="Home" />
         <SidebarRow Icon={HashtagIcon} title="Explore" />
         <SidebarRow Icon={BellIcon} title="Notifications" />
         <SidebarRow Icon={MailIcon} title="Messages" />
         <SidebarRow Icon={BookmarkIcon} title="Bookmarks" />
         <SidebarRow Icon={CollectionIcon} title="Lists" />
-        <SidebarRow Icon={UserIcon} title="Sign In" />
+        <SidebarRow onClick={session ? signOut : signIn } Icon={UserIcon} title={session ? "Sign Out" : "Sign In"} />
         <SidebarRow Icon={DotsCircleHorizontalIcon} title="More" />
     </div>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,35 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import { Toaster } from 'react-hot-toast'
 import Feed from '../components/Feed'
 import Sidebar from '../components/Sidebar'
+import Widgets from '../components/Widgets'
+import { Tweet } from '../typings'
+import { fetchTweets } from '../utils/fetchTweets'
 
-const Home: NextPage = () => {
+interface Props {
+  tweets: Tweet[]
+}
+
+const Home = ({ tweets }: Props) => {
+  console.log(tweets)
+
   return (
-    <div className="">
+    <div className="mx-auto lg:max-w-6xl max-h-screen overflow-hidden">
       <Head>
         <title>Twitter-Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Hello World</h1>
+      <Toaster />
 
-      <main>
+      <main className='grid grid-cols-9'>
         
         <Sidebar />
 
-        <Feed /> 
+        <Feed tweets={tweets} /> 
 
-        {/* Widgets */}
+        <Widgets />
 
       </main>
 
@@ -29,3 +38,14 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  
+  const tweets = await fetchTweets()
+
+  return {
+    props: {
+      tweets,
+    }
+  }
+}
